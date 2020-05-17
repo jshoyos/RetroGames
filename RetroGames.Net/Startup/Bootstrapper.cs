3e6b27e05e79f72a1545fad7cfb6ade437920370
Juan Hoyos <juanse.jsh@gmail.com>
2020-05-16 21:39:28 -0400

    created project and setup bootsrapper

---
+++ RetroGames.Net/Startup/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace RetroGames.Net.Startup
+{
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindow>().AsSelf();
+
+            return builder.Build();
+        }
+    }
+}
